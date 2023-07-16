8b5af9c9906b011267e94443ae98b0e2267e1692
princekr96 <princekr700@gmail.com>
2023-07-17 00:12:08 +0530

    created a counter with test

---
+++ src/components/counter/counter.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <p className="count-val">{counter}</p>
+        <button onClick={() => setCounter((prevState) => prevState + 1)}>
+          +
+        </button>
+        <button onClick={() => setCounter((prevState) => prevState - 1)}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/components/counter/counter.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Counter from "./counter";
+
+test("Counter", () => {
+  render(<Counter />);
+
+  const plusBtn = screen.getByRole("button", { name: "+" });
+  expect(plusBtn).toBeInTheDocument();
+
+  const minusBtn = screen.getByRole("button", { name: "-" });
+  expect(minusBtn).toBeInTheDocument();
+
+  fireEvent.click(plusBtn);
+  expect(screen.getByText("1")).toBeInTheDocument();
+
+  fireEvent.click(plusBtn);
+  fireEvent.click(plusBtn);
+  expect(screen.getByText("3")).toBeInTheDocument();
+});

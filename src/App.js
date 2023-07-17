a871bccd3c7bef6b2668147376b9ac626c870dcc
princekr96 <princekr700@gmail.com>
2023-07-17 22:59:40 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import "./App.css";
 import Counter from "./components/counter/counter";
+import Switch from "./components/switch";
 
 function App() {
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Switch />,
+    },
+    {
+      path: "/counter",
+      element: <Counter />,
+    },
+  ]);
+
   return (
     <div className="App">
-      <h2>Counter</h2>
-      <Counter />
+      <h2>React Testing Demo</h2>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/switch/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Switch = () => {
+  return (
+    <Link to="/counter">
+      <button>Click</button>
+    </Link>
+  );
+};
+
+export default Switch;

bb391f03a1d9e0a6af541f73aedfbe7b3f83549f
princekr96 <princekr700@gmail.com>
2023-07-21 18:15:31 +0530

    added route again for users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Todo from "./components/todo";
 import "./App.css";
 import UserLists from "./components/user-lists";
 
 function App() {
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Todo />,
+    },
+    {
+      path: "/users",
+      element: <UserLists />,
+    },
+  ]);
+
   return (
     <div className="App">
       <h2>React Testing Demo</h2>
-      <Todo />
-      <UserLists />
+      <RouterProvider router={router} />
     </div>
   );
 }
